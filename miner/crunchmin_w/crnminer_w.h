eb04957bb4743117a105d1bedf293d20e22930dc
Kade Burnside <cadellapowell61@gmail.com>
2018-07-05 19:22:10 +1200

    Still quite unfinished, just haven't committed anything in a while

--- miner/crunchmin_w/crnminer_w.h
+++ miner/crunchmin_w/crnminer_w.h
@@ -32,8 +32,12 @@ along with CrunchNet. If not, see <http://www.gnu.org/licenses/>.
 // Windows API include
 #include <ShlObj.h>
 
+BOOL isMinerRunning = FALSE; // Miner status
+
 HANDLE hConsole; // Console output handle
 DWORD charCount; // Characters written to console
+
+LPCWSTR szTitle = L"CrunchNet Miner"; // Default window title
 
 STARTUPINFO si = { 0 }; // Startup info structure
 PROCESS_INFORMATION pi = { 0 }; // Process info structure
@@ -41,7 +45,152 @@ PROCESS_INFORMATION pi = { 0 }; // Process info structure
 SYSTEM_INFO sysinfo; // System info structure
 SYSTEMTIME systime; // System time structure
 
-WSADATA wsa; // WinSock implementation structure
+FILE* fiop; // File IO pointer
+LPBYTE configBuf; // Configuration file buffer
+
+#define MAX_NAME_LENGTH 64 // Maxmium length for name (username, miner name)
+
+// Miner configuration
+LPWSTR accountName, minerName, saveLocation; // Username, miner name and save location
+DWORD64 accountId, walletId, storageAlloc; // Account ID, wallet ID and storage space to be allocated (in bytes)
+DWORD maxProcesses; // CrunchNet applications that can run at once (limit is SYSTEM_INFO.dwNumberOfProcessors)
+
+// Time macros
+#define HOUR systime.wHour
+#define MIN systime.wMinute
+#define SEC systime.wSecond
+
+// "Cannot edit configuration while miner is active!" error message macro
+#define M_ACTIVE_ERR(hWnd) if(isMinerRunning){ MessageBox(hWnd, L"Cannot edit configuration while miner is active!",\
+szTitle, MB_ICONERROR | MB_APPLMODAL); }else
+
+// Prints time to console
+void WriteTime(int destination) {
+	WCHAR time[11]; // Time string
+	GetSystemTime(&systime); // Gets system time
+	// Converts time to string
+	if (HOUR < 10) {
+		if (MIN < 10) {
+			if (SEC < 10) {
+				swprintf(time, 11, L"[0%d:0%d:0%d]", HOUR, MIN, SEC);
+			}
+			else {
+				swprintf(time, 11, L"[0%d:0%d:%d]", HOUR, MIN, SEC);
+			}
+		}
+		else {
+			if (SEC < 10) {
+				swprintf(time, 11, L"[0%d:%d:0%d]", HOUR, MIN, SEC);
+			}
+			else {
+				swprintf(time, 11, L"[0%d:%d:%d]", HOUR, MIN, SEC);
+			}
+		}
+	}
+	else {
+		if (MIN < 10) {
+			if (SEC < 10) {
+				swprintf(time, 11, L"[%d:0%d:0%d]", HOUR, MIN, SEC);
+			}
+			else {
+				swprintf(time, 11, L"[%d:0%d:%d]", HOUR, MIN, SEC);
+			}
+		}
+		else {
+			if (SEC < 10) {
+				swprintf(time, 11, L"[%d:%d:0%d]", HOUR, MIN, SEC);
+			}
+			else {
+				swprintf(time, 11, L"[%d:%d:%d]", HOUR, MIN, SEC);
+			}
+		}
+	}
+
+	if (destination == NULL) {
+		WriteConsole(hConsole, time, 11, &charCount, NULL); // Prints time string to console
+	}
+	else {
+		fwprintf(fiop, L"%s", time); // Saves time string to log file
+	}
+}
+
+// Prints message to console
+void WriteMessage(LPCWSTR msg, DWORD cch, int destination) {
+	if (destination == NULL) {
+		WriteTime(NULL); // Prints time
+		WriteConsole(hConsole, msg, cch, &charCount, NULL); // Prints message
+		WriteConsole(hConsole, L"\n", 1, &charCount, NULL); // Prints line break
+	}
+	else if (destination == 1) {
+		WriteTime(1); // Saves time to log file
+		fwprintf(fiop, L" %s\n", msg); // Saves message to log file
+	}
+	else {
+		WriteTime(NULL); // Prints time
+		WriteConsole(hConsole, msg, cch, &charCount, NULL); // Prints message
+		WriteConsole(hConsole, L"\n", 1, &charCount, NULL); // Prints line break
+		WriteTime(1); // Saves time to log file
+		fwprintf(fiop, L" %s\n", msg); // Saves message to log file
+	}
+}
+
+// Makes 2 errors (one on the console, one as a message box)
+void WriteError(HWND hWnd, LPCWSTR msg, DWORD cch) {
+	WriteMessage(msg, cch, 2); // Prints error
+	MessageBox(hWnd, msg, szTitle, MB_ICONERROR | MB_APPLMODAL); // Shows error message box
+}
+
+// Resets miner if error occurs
+void ResetMiner(HWND hWnd, HWND minerToggle) {
+	isMinerRunning = FALSE; // Sets miner status to inactive
+	// Saves end message to log file
+	fwprintf(fiop, L"Log ended at %d/%d/%d ", systime.wDay, systime.wMonth, systime.wYear);
+	WriteTime(1); // Saves current time to log file
+	fwprintf(fiop, L"\n\n"); // Saves line break character to log file
+	fclose(fiop); // Closes log file stream
+	SetWindowText(hWnd, L"CrunchNet Miner (Inactive)"); // Changes text of dashboard window
+	SetWindowText(minerToggle, L"Start miner"); // Changes text of miner toggle button
+}
+
+// Starts miner
+void StartMiner(HWND hWnd, HWND minerToggle) {
+	isMinerRunning = TRUE; // Sets miner status to active
+	SetWindowText(hWnd, L"CrunchNet Miner (Active)"); // Changes text of dashboard window
+	SetWindowText(minerToggle, L"Stop miner"); // Changes text of miner toggle button
+
+	// Miner initialization
+	GetSystemTime(&systime); // Gets current time
+
+	// Opens log file
+	if (!(_wfopen_s(&fiop, L"log.txt", L"a"))) {
+		// Saves start message to log file
+		fwprintf(fiop, L"Log started at %d/%d/%d ", systime.wDay, systime.wMonth, systime.wYear);
+		WriteTime(1);
+		fwprintf(fiop, L"\n"); // Prints line break character
+	}
+	else {
+		WriteError(hWnd, L"Could not open log file.", 24); // Prints error
+		ResetMiner(hWnd, minerToggle);
+		return;
+	}
+	
+	WriteMessage(L"Initializing miner...", 21, 2); // Prints initialization message
+
+	// Checks for Internet connection
+	if (!(InternetGetConnectedState(NULL, NULL))) {
+		WriteError(hWnd, L"There is no Internet connection. Reconnect and try again.", 57); // Prints error
+		ResetMiner(hWnd, minerToggle); // Resets miner
+		return;
+	}
+}
+
+// Stops miner
+void StopMiner(HWND hWnd, HWND minerToggle) {
+	/* Prompt code goes here */
+
+	WriteMessage(L"Miner has stopped.", 22, 2); // Prints miner termination message to console and log file
+	ResetMiner(hWnd, minerToggle); // Calls ResetMiner function (avoids code duplication)
+}
 
 #endif
 
--- miner/updcheck_w.h
+++ miner/updcheck_w.h
@@ -23,16 +23,85 @@ along with CrunchNet. If not, see <http://www.gnu.org/licenses/>.
 #ifndef UPDCHECK_W_H
 #define UPDCHECK_W_H
 
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+// Standard library include
+#include <string.h>
+
 // Windows API include
 #include <WinSock2.h> // Includes Windows.h
+#include <WinInet.h> // InternetGetConnectedState function
 
 // Sets defenition of NULL to 0
 #undef NULL
 #define NULL 0
 
+#define FAIL 2 // Defines fail code
+
 // Update check function
-BOOL CheckForUpdate(WSADATA* wsa){
-	return FALSE;
+BOOL CheckForUpdate(){
+	BOOL rVal = FALSE; // Update status (value to be returned)
+	
+	WSADATA wsa; // WinSock implementation structure
+	SOCKET sc; // TCP/IP socket
+
+	// Checks if there is an internet connection
+	if (!(InternetGetConnectedState(NULL, NULL))) {
+		// Shows warning message box if disconnected
+		MessageBox(NULL, L"For CrunchNet to work, there must be an active Internet connection.\n\nCrunchNet will now start in offline mode. You will be able to edit the miner configuration, but you will not be able to mine or refresh wallet list.", L"CrunchNet Updater", MB_ICONWARNING);
+		return FALSE;
+	}
+
+	// Initializes WinSock
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		// Show error message box if initialization failed
+		MessageBox(NULL, L"WinSock initialization failed!", L"CrunchNet Updater", MB_ICONERROR);
+		return FAIL;
+	}
+
+	HOSTENT* host; // Database entry description for a single host
+	IN_ADDR** addrlist; // List of addresses from name server
+	char ip[100]; // IP address
+
+	// Gets host IP address
+	if ((host = gethostbyname("www.google.com")) == NULL) {
+		// Show error message box if gethostbyname failed
+		MessageBox(NULL, L"Could not retrieve IP address from DNS server", L"CrunchNet Updater", MB_ICONERROR);
+		return FAIL;
+	}
+
+	addrlist = (IN_ADDR**)host->h_addr_list; // Casts address list to IN_ADDR
+	// Return the first address
+	for (int i = 0; addrlist[i] != NULL; i++) {
+		strcpy_s(ip, 100, inet_ntoa(*addrlist[i]));
+	}
+
+	SOCKADDR_IN addr; // Address to be binded to socket
+	int addrlen = sizeof(addr); // Length of address (required for accept call)
+	addr.sin_addr.s_addr = inet_addr(ip); // IP address
+	addr.sin_port = htons(80); // Port
+	addr.sin_family = AF_INET; // IPv4 socket
+
+	// Creates socket
+	if ((sc = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+		// Show error message if socket creation failed
+		MessageBox(NULL, L"Socket creation failed!", L"CrunchNet Updater", MB_ICONERROR);
+		return FAIL;
+	}
+
+	// Connects to update server (Updates are distributed via the download website, not the blockchain itself)
+	if (connect(sc, (SOCKADDR*)&addr, addrlen) != 0) {
+		// Show error message box if connection failed
+		MessageBox(NULL, L"Could not connect to update server", L"CrunchNet Updater", MB_ICONERROR);
+		return FAIL;
+	}
+
+	/* Request code goes here */
+
+	closesocket(sc); // Closes socket
+	WSACleanup(); // Unloads WinSock library
+	
+	return rVal; // Returns update status
 }
 
 #endif
